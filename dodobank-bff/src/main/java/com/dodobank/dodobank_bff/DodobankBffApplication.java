e8d8a644cc73491a7ea7172523b6f4131991bca7
Koffi Eli KPONBLANOU <koffi.eli.kponblanou@accenture.com>
2024-10-24 21:33:07 +0400

    setup(auth): initialize the bff project

---
+++ dodobank-bff/src/main/java/com/dodobank/dodobank_bff/DodobankBffApplication.java
@@ -0,0 +1,13 @@
+package com.dodobank.dodobank_bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodobankBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DodobankBffApplication.class, args);
+	}
+
+}
---
+++ dodobank-bff/src/test/java/com/dodobank/dodobank_bff/DodobankBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dodobank.dodobank_bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodobankBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
